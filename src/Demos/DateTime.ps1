22d4399fc61013aa6c6312f166a4210f31be4f88
LaurentDardenne <ladardenne@orange.fr>
2022-08-24 13:46:19 +0200

    Translation

--- src/Demos/DateTime.ps1
+++ src/Demos/DateTime.ps1
@@ -2,15 +2,6 @@
 
 
 #MSDN documentation
-#
-#French
-  # Choisir entre DateTime, DateTimeOffset, TimeSpan et TimeZoneInfo :
-  #  https://msdn.microsoft.com/fr-fr/library/bb384267(v=vs.110).aspx
-  #
-  # Conversion entre DateTime et DateTimeOffset :
-  #  https://msdn.microsoft.com/fr-fr/library/bb546101(v=vs.110).aspx
-
-#English
   # Choosing between DateTime, DateTimeOffset, TimeSpan, and TimeZoneInfo :
   #  https://msdn.microsoft.com/en-us/library/bb384267(v=vs.110).aspx
   #
@@ -21,46 +12,46 @@
 Function Demo {
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Scope="Function")]
 param()
-  Write-host "`r`nToujours enregistrer une date au format UTC !" -Fore Green
+  Write-host "`r`nAlways save a date in UTC format!" -Fore Green
   $Date=Get-Date
   $UTC=$Date.ToUniversalTime()
-  #Remplace au lieu de  :
+  # Instead  :
   # $timeZone = [TimeZone]::CurrentTimeZone
   # $timeZone.ToUniversalTime($Date)
 
-  Write-host ("`r`nDate locale={0}" -F $Date)  -Fore Green
+  Write-host ("`r`nDate ={0}" -F $Date)  -Fore Green
   Write-host ("`r`nDate UTC={0}" -F $UTC)  -Fore Green
   Write-host ("`r`nDate UTC to LocalTime={0}" -F ($UTC.ToLocalTime()))  -Fore Green
 
   $TimeZone=[TimeZoneInfo]::FindSystemTimeZoneById('Eastern Standard Time')
   Write-host ("`r`nEastern Standard Time : {0}" -F [TimeZoneInfo]::ConvertTimeFromUtc($UTC,$TimeZone))
-  Write-host ("`r`nHier {0} (affichage avec la culture courante)" -F ([DateTime]::Now.PreviousDay()))
-  Write-host "`r`nHier $([DateTime]::Now.PreviousDay()) (affichage avec la culture invariant (US par défaut))"
-  #Lorsque l'on résoud une variable entre des doubles quotes,
-  # PowerShell formate les nombres et les dates avec la culture invariant et pas la culture en cours (Get-Culture)
+  Write-host ("`r`nHier {0} (display with current culture)" -F ([DateTime]::Now.PreviousDay()))
+  Write-host "`r`nHier $([DateTime]::Now.PreviousDay()) (display with invariant culture (default US))"
+  #When resolving a variable between double quotes,
+  # PowerShell formats numbers and dates with the invariant culture and not the current culture (Get-Culture)
 
-  Write-host ("`r`nDemain {0}" -F [DateTime]::Now.NextDay())
-  Write-host ("`r`nAujourd'hui {0}" -F [DateTime]::Now)
-  Write-host ("`r`nAujourd'hui à 11h55 {0}" -F [DateTime]::Now.SetTime(11, 55, 0))
+  Write-host ("`r`nToday {0}" -F [DateTime]::Now)
+  Write-host ("`r`nToday at 11:55 a.m. {0}" -F [DateTime]::Now.SetTime(11, 55, 0))
+  Write-host ("`r`ntomorrow {0}" -F [DateTime]::Now.NextDay())
 
-  Write-Host "`r`nLe type [Int] propose des méthodes simplifiant la construction de date"
-  Write-Host ("`r`nIl y a 3 jours  (3).Days().Ago() : {0}" -F (3).Days().Ago())
-  Write-Host ("`r`nDans 3 jours  (-3).Days().Ago() : {0}" -F (-3).Days().Ago())
-  Write-Host ("`r`Une durée de 3 jours et 14 minutes  (3).Days() + (14).Minutes()" -F  ((3).Days() + (14).Minutes()) )
+  Write-Host "`r`nThe [Int] type offers methods simplifying date construction."
+  Write-Host ("`r`n3 days ago (3).Days().Ago() : {0}" -F (3).Days().Ago())
+  Write-Host ("`r`nIn 3 days  (-3).Days().Ago() : {0}" -F (-3).Days().Ago())
+  Write-Host ("`r`A duration of 3 days and 14 minutes (3).Days() + (14).Minutes()" -F ((3).Days() + (14).Minutes()) )
 
-   #ToString() utilise la culture courante pour le formatage de DateTime
-   #Peut renvoyer une date de l'année suivante, selon la date d'interrogation
-  Write-host "`r`nRenvoi la date du prochain Lundi : $([DateTime]::Now.Next([DayOfWeek]::Monday).ToString())"
+   #ToString() uses the current culture for DateTime formatting
+   #May return a date in the next year, depending on the query date
+  Write-host "`r`nReturns the date of the next Monday : $([DateTime]::Now.Next([DayOfWeek]::Monday).ToString())"
 
-   #Format de date basé sur la culture courante (Fr)
+   #Date format based on the current culture (En)
   $Date ='05/06/2017' -as [Datetime]
-  Write-host "`r`nRenvoi la date du prochain Lundi à partir du lundi 5 juin 2017: $(($Date.Next([DayOfWeek]::Monday)).ToString())"
+  Write-host "`r`nReturns the date of the next Monday from Monday, May 6, 2017: $(($Date.Next([DayOfWeek]::Monday)).ToString())"
 
-  #Dans 15 jours, c'est selon ...
-  Write-host "`r`nDans 2 semaines (jours ouvrables ) : $(([DateTime]::Now.AddDays((2*5-1))).ToString())"
+   #In 15 days, it's according to...
+  Write-host "`r`nIn 2 weeks (working days): $(([DateTime]::Now.AddDays((2*5-1))).ToString())"
 
-  #Ne tient pas compte des jours fériés
-  #-1 pour le jour en cours
-  Write-host "`r`nDans 2 semaines (jours ouvré ) : $(([DateTime]::Now.AddBusinessDays(((2*5)-1))).ToString())"
+   #Does not take public holidays into account
+   #-1 for the current day
+  Write-host "`r`nIn 2 weeks (open days): $(([DateTime]::Now.AddBusinessDays(((2*5)-1))).ToString())"
 }
 . Demo
