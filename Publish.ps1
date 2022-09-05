0aa3db1f557751533ee5402348032fe23d2675f7
LaurentDardenne <ladardenne@orange.fr>
2022-09-05 13:07:03 +0200

    Task BeforePublish

--- Publish.ps1
+++ Publish.ps1
@@ -20,3 +20,28 @@ $Repositories=@{
 $Repositories.$($PsCmdlet.ParameterSetName)
 # Builds the module by invoking psake on the build.psake.ps1 script.
 Invoke-PSake $PSScriptRoot\build.psake.ps1 -taskList Publish -parameters @{"RepositoryName"=$Repositories.$($PsCmdlet.ParameterSetName)}
+
+<#
+# Executes before the Publish task.
+Task BeforePublish -requiredVariables Projectname, OutDir, ModuleName, PublishRepository, Dev_PublishRepository {
+    $ManifestPath="$OutDir\$ModuleName\$ModuleName.psd1"
+    if ( (-not [string]::IsNullOrWhiteSpace($Dev_PublishRepository)) -and ($PublishRepository -eq $Dev_PublishRepository ))
+    {
+        #Increment  the module version for dev repository only
+        Import-Module BuildHelpers
+        $SourceLocation=(Get-PSRepository -Name $PublishRepository).SourceLocation
+        "Get the latest version for '$ProjectName' in '$SourceLocation'"
+        $Version = Get-NextNugetPackageVersion -Name $ProjectName -PackageSourceUrl $SourceLocation
+
+        $ModuleVersion=(Test-ModuleManifest -path $ManifestPath).Version
+        # If no version exists, take the current version
+        $isGreater=$Version -gt $ModuleVersion
+        "Update the module metadata '$ManifestPath' [$ModuleVersion] ? $isGreater "
+        if ($isGreater)
+        {
+           "with the new version : $version"
+           Update-Metadata -Path $ManifestPath  -PropertyName ModuleVersion -Value $Version
+        }
+    }
+}
+#>
