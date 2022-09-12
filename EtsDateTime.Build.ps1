8f4b3612476f29707a02ee1c32fa47acd27448fa
LaurentDardenne <ladardenne@orange.fr>
2022-09-12 14:16:02 +0200

    WIP Enabled tests

--- EtsDateTime.Build.ps1
+++ EtsDateTime.Build.ps1
@@ -27,7 +27,7 @@ if ($PSBoundParameters.ContainsKey('Debug'))
 ###############################################################################
 # Core task implementations. Avoid modifying these tasks.
 ###############################################################################
-task . Build
+task . Test
 
 task Init {
     NewDirectory -Path $ModuleOutDir -TaskName $Task.Name
@@ -127,8 +127,7 @@ task CoreInstall {
     "Module installed into $InstallPath"
 }
 
-task Test -If { return $false } Build, {
- #todo
+task Test Build, {
     if (!(Get-Module Pester -ListAvailable)) {
         "Pester module is not installed. Skipping $($Task.Name) task."
         return
@@ -138,40 +137,40 @@ task Test -If { return $false } Build, {
 
     try {
         Microsoft.PowerShell.Management\Push-Location -LiteralPath $TestRootDir
+        .\Run.ps1
+        # if ($TestOutputFile) {
+        #     $testing = @{
+        #         OutputFile   = $TestOutputFile
+        #         OutputFormat = $TestOutputFormat
+        #         PassThru     = $true
+        #         Verbose      = $VerbosePreference
+        #     }
+        # }
+        # else {
+        #     $testing = @{
+        #         PassThru     = $true
+        #         Verbose      = $VerbosePreference
+        #     }
+        # }
 
-        if ($TestOutputFile) {
-            $testing = @{
-                OutputFile   = $TestOutputFile
-                OutputFormat = $TestOutputFormat
-                PassThru     = $true
-                Verbose      = $VerbosePreference
-            }
-        }
-        else {
-            $testing = @{
-                PassThru     = $true
-                Verbose      = $VerbosePreference
-            }
-        }
+        # # todo To control the Pester code coverage, a boolean $CodeCoverageEnabled is used.
+        # if ($CodeCoverageEnabled) {
+        #     $testing.CodeCoverage = $CodeCoverageFiles
+        # }
 
-        # To control the Pester code coverage, a boolean $CodeCoverageEnabled is used.
-        if ($CodeCoverageEnabled) {
-            $testing.CodeCoverage = $CodeCoverageFiles
-        }
+        # $testResult = Invoke-Pester @testing
 
-        $testResult = Invoke-Pester @testing
+        # Assert ( $testResult.FailedCount -eq 0) 'One or more Pester tests failed, build cannot continue.'
 
-        Assert ( $testResult.FailedCount -eq 0) 'One or more Pester tests failed, build cannot continue.'
-
-        if ($CodeCoverageEnabled) {
-            $testCoverage = [int]($testResult.CodeCoverage.NumberOfCommandsExecuted /
-                                  $testResult.CodeCoverage.NumberOfCommandsAnalyzed * 100)
-            "Pester code coverage on specified files: ${testCoverage}%"
-        }
+        # if ($CodeCoverageEnabled) {
+        #     $testCoverage = [int]($testResult.CodeCoverage.NumberOfCommandsExecuted /
+        #                           $testResult.CodeCoverage.NumberOfCommandsAnalyzed * 100)
+        #     "Pester code coverage on specified files: ${testCoverage}%"
+        # }
     }
     finally {
         Microsoft.PowerShell.Management\Pop-Location
-        Remove-Module $ModuleName -ErrorAction SilentlyContinue -Force
+        # todo unload DLL: Remove-Module $ModuleName -ErrorAction SilentlyContinue -Force
     }
 }
 
