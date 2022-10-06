024ee5a04b04d2796cafcc1f4e9612cb60d1c088
LaurentDardenne <ladardenne@orange.fr>
2022-10-06 10:50:26 +0200

    Remove comment

--- build.ps1
+++ build.ps1
@@ -14,4 +14,4 @@ Write-Host "Build the delivery for the EtsDatetime module."
 $local:Verbose=$PSBoundParameters.ContainsKey('Verbose')
 $local:Debug=$($PSBoundParameters.ContainsKey('Debug'))
 Invoke-Build -File "$PSScriptRoot\EtsDatetime.build.ps1" -Configuration $PsCmdlet.ParameterSetName -Environnement $Environnement -Verbose:$Verbose -Debug:$Debug
-#-Task Build
+
